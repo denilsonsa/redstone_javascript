2ad5c116aac414fa3e83c7ca3554d79da6e3084a
Denilson Sá <denilsonsa@gmail.com>
2011-03-08 14:03:07 -0300

    Some code, some refactor... I'm still not satisfied with this.

--- generate_tiles_css.py
+++ generate_tiles_css.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vi:ts=4 sw=4 et
+# vi:ts=16 sw=4 et
 
 from __future__ import print_function
 
@@ -12,11 +12,35 @@ tile_height = 32
 
 css_header = '''@charset "UTF-8";
 
-.tile {
+.cell {
     background-image: url("tiles.png");
     background-repeat: no-repeat;
 }
 '''
+
+tile_map = [
+['air',	'button_s',	'button_w',	'button_n',	'button_e',	None,	'plate_stone',	'dirt'],
+['lever_up',	'lever_s',	'lever_w',	'lever_n',	'lever_e',	None,	'plate_wood',	'sand'],
+['torch_up',	'torch_s',	'torch_w',	'torch_n',	'torch_e',	'wire_nsew',	'wire_ew',	'wire_ns'],
+['wire_new',	'wire_nse',	'wire_sew',	'wire_nsw',	'wire_ne',	'wire_se',	'wire_sw',	'wire_nw'],
+['torch_up',	'torch_s',	'torch_w',	'torch_n',	'torch_e',	'wire_nsew',	'wire_ew',	'wire_ns'],
+['wire_new',	'wire_nse',	'wire_sew',	'wire_nsw',	'wire_ne',	'wire_se',	'wire_sw',	'wire_nw'],
+['wool_F',	'wool_E',	'wool_D',	'wool_C',	'wool_B',	'wool_A',	'wool_9',	'wool_8',],
+['wool_7',	'wool_6',	'wool_5',	'wool_4',	'wool_3',	'wool_2',	'wool_1',	'wool_0',],
+['repeater_n',	'repeater_e',	'repeater_s',	'repeater_w',	'repeater_n',	'repeater_e',	'repeater_s',	'repeater_w',],
+]
+
+on_off_map = [
+'        ',
+'        ',
+'00000000',
+'00000000',
+'11111111',
+'11111111',
+'        ',
+'        ',
+'00001111',
+]
 
 def main():
     print(css_header)
@@ -24,11 +48,15 @@ def main():
     num_tiles_y = img_height // tile_height
     for y in range(num_tiles_y):
         for x in range(num_tiles_x):
-            print('.tile.t{0} {{ background-position: -{1}px -{2}px; }}'.format(
-                y * num_tiles_x + x,
-                x * tile_width,
-                y * tile_height
-            ))
+            name = tile_map[y][x]
+            if name:
+                is_on = (on_off_map[y][x] == '1')
+                print('.cell.cell_{0}{1} {{ background-position: -{2}px -{3}px; }}'.format(
+                    name,
+                    '.on' if is_on else '',
+                    x * tile_width,
+                    y * tile_height
+                ))
 
 if __name__ == "__main__":
     main()
