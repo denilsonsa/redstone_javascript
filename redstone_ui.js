b11b6d046a100e9f6b99325e3013736bc1978a47
Denilson Sá <denilsonsa@gmail.com>
2011-03-09 18:08:16 -0300

    Added neat collapsable fieldsets!

--- redstone_ui.js
+++ redstone_ui.js
@@ -9,6 +9,37 @@ var main_tabs;
 var dynamic_mapgrid_stylesheet;
 
 
+function toggle_collapsable_fieldset(ev) {
+	if (ev.target.tagName.toLowerCase() !== 'legend') {
+	//	console.error('toggle_collapsable_fieldset() called with target=', ev.target);
+		return;
+	}
+	var fieldset = ev.target.parentNode;
+	if (fieldset.tagName.toLowerCase() !== 'fieldset') {
+	//	console.error('toggle_collapsable_fieldset() called, but parentNode is not a fieldset: ', fieldset);
+		return;
+	}
+
+	if (fieldset.className.indexOf('collapsable') > -1) {
+		if (fieldset.className.indexOf('collapsed') > -1) {
+			remove_class(fieldset, 'collapsed');
+		} else {
+			add_class(fieldset, 'collapsed');
+		}
+	}
+
+	ev.preventDefault();
+}
+function init_collasable_fieldsets() {
+	// Adding "Collapsable fieldsets" event handlers
+	var fs_legends = document.querySelectorAll('fieldset.collapsable > legend');
+	for (i = 0; i < fs_legends.length; i++) {
+		fs_legends[i].addEventListener('click', toggle_collapsable_fieldset, false);
+	}
+}
+window.addEventListener('load', init_collasable_fieldsets, false);
+
+
 
 function init_dynamic_mapgrid_stylesheet() {
 	// *sigh*... I wish document.createCSSStyleSheet() was supported...
